cdfe8342f04e4df4d1f2f896b03d5c664275fb54
Peter <pdoan017@gmail.com>
2019-06-03 22:03:39 -0400

    Added functions for all 3 x-y-z directions. Added function for buoyancy forces

--- 02-Code/functions.h
+++ 02-Code/functions.h
@@ -19,38 +19,54 @@ namespace SaturnWheel
   // Assumes that x2 > x1
   #define LIMIT(x1, x2, y) ((y>x2)?x2:((y<x1)?x1:y))
 
+  // Calculation of gravity force
+  #define F_GRAVITY(m, a) ((m)*(a))
+
   // Calculation of acceleration due to drag
   //
-  // The drag force is determined using the equation
-  // F=0.5*rho*u^2*area_ref*c_drag
-  //
-  // The resultant force is equivalent to F=m*a
-  #define ACCEL_DRAG(rho, u, c_drag) ((0.5*rho*u^2*area_ref*c_drag)/m)
-  #define COEFF_DRAG(rho, u, a)      ((m*a)/(0.5*rho*u^2*area_ref))
 
   class Model
   {
     static const float c_drag[2] = [0.12, 0.34]; // Table of Drag constants at closed and open positions
-    static float m; // Mass of spacecraft                                                 [kg]
-    static float u_air; // Velocity of surrounding air                                    [m/s]
-    static float u_sc_cur; // Current velocity of spacecraft                              [m/s]
-    static float u_sc_req; // Requested velocity of spacecraft                            [m/s]
-    static float a_sc_cur; // Current acceleration of spacecraft                          [m/s^2]
-    static float a_sc_req; // Requested acceleration of spacecraft                        [m/s^2]
-    static float t_air; // Temperature of surrounding air                                 [Celsius]
-    static const float area_ref = 2.356; // Reference area for drag equation              [m^2]
-    static float d_shutter_act_cur; // Current position of shutter actuator semi-halves   [%]
-    static float d_shutter_act_req; // Requested position of shutter actuator semi-halves [%]
-    static float r_shutter_act_req; // Requested rate for shutter to open/close           [%/s]
-    static float c_drag_cur; // Current drag coefficient                                  [-]
-    static float c_drag_req; // Requested drag coefficient                                [-]
+    static float m;                       // Mass of spacecraft                                 [kg]
+    static float d_alt;                   // Altitude of spacecraft from center of planet       [m]
+    static float u_air;                   // Velocity of surrounding air                        [m/s]
+    static float a_gravity;               // Acceleration due to gravity                        [m/s^2]
+    static float u_sc_cur[3];             // Current velocity of spacecraft                     [m/s]
+    static float u_sc_req[3];             // Requested velocity of spacecraft                   [m/s]
+    static float a_sc_cur[3];             // Current acceleration of spacecraft                 [m/s^2]
+    static float a_sc_req[3];             // Requested acceleration of spacecraft               [m/s^2]
+    static float f_sc_cur[3];             // Current forces acting on spacecraft                [N]
+    static float t_air;                   // Temperature of surrounding air                     [Celsius]
+    static float rho_cur;                 // Current density of surrounding air                 [kg/m^3]
+    static float rho_prev;                // Previous density of surrounding air                [kg/m^3]
+    static const float area_ref = 2.356;  // Reference area for drag equation                   [m^2]
+    static float d_shutter_act_cur[5];    // Current position of shutter actuator semi-halves   [%]
+                                            // Indices:
+                                            // 0 - top
+                                            // 1 - quad 1
+                                            // 2 - quad 2
+                                            // 3 - quad 3
+                                            // 4 - quad 4
+    static float d_shutter_act_req[5];    // Requested position of shutter actuator semi-halves [%]
+    static float r_shutter_act_req[5];    // Requested rate for shutter to open/close           [%/s]
+    static float c_drag_cur;              // Current drag coefficient                           [-]
+    static float c_drag_req;              // Requested drag coefficient                         [-]
+    static float v_top;                   // Volume of top of spacecraft                        [m^3]
+
+    float getDensity(void);
+    float getGravity(void);
+    float getForceBuoy(void);
+    float getForceGrav(void);
+    float getForceDrag(float u, float c_drag);
+    float getCoeffDrag(float u, float a);
 
   public:
-    void update();
-    void getAtm();
-    void calcCurrIter();
-    void calcReq();
-    void calcNextIter();
+    void doUpdate(void);
+    void getAtm(void);
+    void calcCurrIter(void);
+    void calcReq(void);
+    void calcNextIter(void);
   }
 }
 
